656f2f04024b1d5a97cad3d9d14ca65650208c70
Kavit2102 <kavitdesai212002@gmail.com>
2024-04-25 13:59:13 +0530

    Auto commit on save

--- program2.py
+++ program2.py
@@ -4,4 +4,33 @@ class Solution(object):
         :type s: str
         :rtype: int
         """
-        pass
+        roman_values = {
+            'I': 1,
+            'V': 5,
+            'X': 10,
+            'L': 50,
+            'C': 100,
+            'D': 500,
+            'M': 1000
+        }
+        
+        total = 0
+        prev_value = 0
+        
+        for char in reversed(s):
+            value = roman_values[char]
+            if value < prev_value:
+                total -= value
+            else:
+                total += value
+            prev_value = value
+        
+        return total
+
+# Example usage:
+solution = Solution()
+print(solution.romanToInt("III"))  # Output: 3
+print(solution.romanToInt("IV"))   # Output: 4
+print(solution.romanToInt("IX"))   # Output: 9
+print(solution.romanToInt("LVIII"))  # Output: 58
+print(solution.romanToInt("MCMXCIV"))  # Output: 1994
